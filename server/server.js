6e3d10a61e9229e9d3b37382c77933d36797f76d
suhas5979 <suhassuryavanshi5979@gmail.com>
2021-10-15 16:28:22 +0530

    boilerplate setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+//
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  socket.on("get-document", (documentId) => {
+    const data = "hiii";
+    socket.join(documentId);
+    socket.emit("load-document", data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+  });
+});
